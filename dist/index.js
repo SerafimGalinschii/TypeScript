6006efeecc7068db18a03a037366ca2e37952dbe
SerafimGalinschii <serafim.galinschii@gmail.com>
2024-06-18 14:39:14 +0300

    Create Clases in TS

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,47 @@
 "use strict";
-let age = 20;
-if (age < 50) {
-    age += 10;
+// Type Aliase
+let employee = {
+    id: 1,
+    name: "Mosh",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+function kgToLbs(weight) {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
 }
+kgToLbs(10);
+kgToLbs("10");
+let textBox = {
+    drag: () => { },
+    resize: () => { },
+};
+let qunatity = 100;
+let metric = "cm";
+// Nullable Types
+function greet(name) {
+    if (name) {
+        console.log(name.toUpperCase());
+    }
+    else {
+        console.log("Hola!");
+    }
+}
+greet(undefined);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday.getFullYear());
+// Nullish coslescing operator
+let speed = null;
+let irde = {
+    speed: speed !== null && speed !== void 0 ? speed : 30,
+};
+// Type Assertions
+let phone = document.getElementById("phone");
+phone.value;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,84 @@
-let age: number = 20;
+// Type Aliase
 
-if (age < 50) {
-  age += 10;
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let employee: Employee = {
+  id: 1,
+  name: "Mosh",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+
+function kgToLbs(weight: number | string): number {
+  if (typeof weight === "number") return weight * 2.2;
+  else return parseInt(weight) * 2.2;
 }
+
+kgToLbs(10);
+kgToLbs("10");
+
+// Intersection type
+
+type Draggable = {
+  drag: () => void;
+};
+
+type Resizable = {
+  resize: () => void;
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {},
+};
+
+// Literal types
+
+type Quantity = 50 | 100;
+let qunatity: Quantity = 100;
+
+type Metric = "cm" | "inch";
+let metric: Metric = "cm";
+
+// Nullable Types
+
+function greet(name: string | null | undefined) {
+  if (name) {
+    console.log(name.toUpperCase());
+  } else {
+    console.log("Hola!");
+  }
+}
+
+greet(undefined);
+
+// Optional Chaining
+type Customer = {
+  birthday: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(1);
+
+console.log(customer?.birthday.getFullYear());
+
+// Nullish coslescing operator
+let speed: number | null = null;
+let irde = {
+  speed: speed ?? 30,
+};
+
+// Type Assertions
+let phone = document.getElementById("phone") as HTMLInputElement;
+
+phone.value;
